84b2d841c3811543b0089da6c2b573f7a8d7e9ac
Bruno Saturnino Alves <brunosaturnino55@gmail.com>
2022-04-26 20:17:34 -0300

    day10js

--- animais_fantasticos/script.js
+++ animais_fantasticos/script.js
@@ -1,4 +1,4 @@
-// Seleção de Elementos
+// ***************** Seleção de Elementos ********************
 // const animais = document.getElementById('animais');
 // console.log(animais);
 
@@ -34,26 +34,111 @@
 //     console.log(item);
 // });
 
-// Exercicio
+// *********************** Exercicio ***************************
 
-let todasImg = document.querySelectorAll('img');
-console.log(todasImg);
+// let todasImg = document.querySelectorAll('img');
+// console.log(todasImg);
 
-let onlyImg = document.querySelectorAll('img[src^="img/img"]');
-console.log(onlyImg);
+// let onlyImg = document.querySelectorAll('img[src^="img/img"]');
+// console.log(onlyImg);
 
-let linksInternos = document.querySelectorAll('[href^="#"]');
-console.log(linksInternos);
+// let linksInternos = document.querySelectorAll('[href^="#"]');
+// console.log(linksInternos);
 
-let h2 = document.querySelector('.animais-descricao h2');
-console.log(h2);
+// let h2 = document.querySelector('.animais-descricao h2');
+// console.log(h2);
 
-let todosP = document.querySelectorAll('p');
-console.log(todosP);
+// let todosP = document.querySelectorAll('p');
+// console.log(todosP);
 
-let ultimoP = todosP[--todosP.length];
-console.log(ultimoP);
+// let ultimoP = todosP[--todosP.length];
+// console.log(ultimoP);
 
 // let auxP = Array.from(todosP);
 // let ultimoP = auxP.pop();
 // console.log(ultimoP);
+
+// forEach e Arrow Function
+
+// const imgs = document.querySelectorAll('img');
+
+// let i = 0;
+
+// imgs.forEach(function(item, index, array){
+//     console.log(array);
+// });
+
+// const titulos = document.getElementsByClassName('titulo');
+// const titulosArray = Array.from(titulos);
+
+// titulosArray.forEach(function(item, index, array){
+//     console.log(item, index, array);
+// });
+
+// const imgs = document.querySelectorAll('img');
+
+// imgs.forEach((item) => {
+//     console.log(item);
+// });
+
+// const allP = document.querySelectorAll('p');
+// console.log(allP);
+
+// allP.forEach((p) => {
+//     console.log(p.innerText);
+// });
+
+// ********************* Classes e Atrubutos *********************************
+
+// const menu = document.querySelector('.menu');
+
+// menu.classList.add('ativo');
+// menu.classList.remove('azul');
+// //menu.classList.toggle('azul');
+
+// if(menu.classList.contains('azul')) {
+//     menu.classList.add('possui-azul');
+// } else  {
+//     menu.classList.add('nao-possui-azul');
+// }
+
+//menu.className = `${menu.className} inativo preto`;
+//menu.className += ' vermelho';
+
+//console.log(menu.className);
+
+// const animais = document.querySelector('.animais');
+// console.log(animais.attributes['data-texto']);
+
+// const img = document.querySelector('img');
+// const srcImg = img.getAttribute('alt');
+// img.setAttribute('alt','É uma raposa');
+// const possuiAlt = img.hasAttribute('alt');
+// console.log(possuiAlt);
+
+// console.log(srcImg);
+
+const menuItens = document.querySelectorAll('.menu li');
+console.log(menuItens);
+
+menuItens.forEach((item) => {
+    item.classList.add('ativo');
+});
+console.log(menuItens);
+
+for(let i = 1; i <= (menuItens.length - 1); i++) {
+    menuItens[i].classList.remove('ativo');
+}
+console.log(menuItens);
+
+const imgs = document.querySelectorAll('img');
+console.log(imgs);
+imgs.forEach((img) => {
+    if(img.hasAttribute('alt')) {
+        console.log(`A img ${img.alt} tem o atributo alt`);
+    }
+});
+
+const linkExterno = document.querySelector('.menu [href^="https"]');
+console.log(linkExterno);
+linkExterno.setAttribute('href','https://www.google.com');
